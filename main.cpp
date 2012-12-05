78ca1e0dbda749b507bd36afb373556c67e1e132
twobit <twobit@lavabit.com>
2012-12-05 23:34:39 +0100

    decode

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@
 #include <string>
 #include <map>
 #include <algorithm>
-
+#include <cmath>
 using namespace std;
+
+#include <assert.h>
 
 template <typename T>
 class matrix {
@@ -40,6 +42,25 @@ public:
 		other.data = data;
 		data = t;
 	}
+	void normalize() {
+		for (size_t x = 0; x < w; x++) {
+			T s = h;
+			for (size_t y = 0; y < h; y++) s += (*this)[y][x];
+			s = 1 / s;
+			for (size_t y = 0; y < h; y++) (*this)[y][x] = ((*this)[y][x] + 1) * s;
+		}
+	}
+	void save(ofstream& file) {
+		file.write((const char*) &h, sizeof(size_t));
+		file.write((const char*) &w, sizeof(size_t));
+		file.write((const char*) data, sizeof(T) * w * h);
+	}
+	void load(ifstream& file) {
+		file.read((char*) &h, sizeof(size_t));
+		file.read((char*) &w, sizeof(size_t));
+		init(h, w);
+		file.read((char*) data, sizeof(T) * w * h);
+	}
 };
 
 
@@ -61,31 +82,19 @@ size_t read_corpus(const string filename,
 		stringstream words(line);
 		vector<size_t> sentence;
 		string word;
-
 		while (words >> word) {
 			size_t id;
 			if (word2id.count(word) == 0) {
 				id = id2word.size();
 				word2id[word] = id;
 				id2word.push_back(word);
-			} else {
-				id = word2id[word];
-			}
+			} else id = word2id[word];
 			sentence.push_back(id);
 		}
 		maxlen = max(maxlen, sentence.size());
 		corpus.push_back(sentence);
 	}
 	return maxlen;
-}
-
-
-void normalize(matrix<float>& m) {
-	for (size_t x = 0; x < m.width(); x++) {
-		float s = 0;
-		for (size_t y = 0; y < m.height(); y++) s += m[y][x];
-		for (size_t y = 0; y < m.height(); y++) m[y][x] /= s;
-	}
 }
 
 
@@ -97,36 +106,17 @@ map<string, size_t>		f_word2id;
 vector<string>			e_id2word;
 vector<string>			f_id2word;
 matrix<float>			dict;
-
-
-void top_ten(const string word) {
-	cout << word << "\n";
-	if (!f_word2id.count(word)) {
-		cout << "\tUnknown word.\n";
-		return;
-	}
-	size_t id = f_word2id[word];
-	vector<size_t> top;
-	float s = 0;
-	for (size_t i = 0; i < e_id2word.size(); i++) {
-		top.push_back(i);
-		s += dict[id][i];
-	}
-	sort(top.begin(), top.end(), [&](size_t a, size_t b) {
-		return dict[id][a] > dict[id][b];
-	});
-	for (size_t i = 0; i < 10; i++) {
-		printf("\t%12.10f   %s\n", dict[id][top[i]] / s, e_id2word[top[i]].c_str());
-	}
-}
+matrix<float>			langmodel;
+matrix<float>			lenmodel;
 
 
 void save() {
-	cout << "Saving...\n";
+	cerr << "Saving...\n";
 	string filename = base + ".meta-" + f_lang + "-to-" + e_lang;
 	ofstream file(filename);
 	if (!file.is_open()) {
 		cerr << "Error opening file " << filename << ".\n";
+		exit(1);
 	}
 
 	file << f_id2word.size() << "\n";
@@ -134,16 +124,19 @@ void save() {
 	file << e_id2word.size() << "\n";
 	for (auto s : e_id2word) file << s << "\n";
 
-	file.write((char*)dict.ptr(), sizeof(float) * f_id2word.size() * e_id2word.size());
+	langmodel.save(file);
+	lenmodel.save(file);
+	dict.save(file);
 }
 
 
 void load() {
-	cout << "Loading...\n";
+	cerr << "Loading...\n";
 	string filename = base + ".meta-" + f_lang + "-to-" + e_lang;
 	ifstream file(filename);
 	if (!file.is_open()) {
 		cerr << "Error opening file " << filename << ".\n";
+		exit(1);
 	}
 
 	size_t len;
@@ -164,8 +157,9 @@ void load() {
 
 	// binary for speed
 	file.seekg(1, ios_base::cur); // skip newline
-	file.read((char*)dict.ptr(), sizeof(float) * f_id2word.size() * e_id2word.size());
-
+	langmodel.load(file);
+	lenmodel.load(file);
+	dict.load(file);
 }
 
 
@@ -176,7 +170,7 @@ void leaving(int sig) {
 
 
 void train(int iterations) {
-	cout << "Reading corpora...\n";
+	cerr << "Reading corpora...\n";
 	vector<vector<size_t>>	e_corpus;
 	vector<vector<size_t>>	f_corpus;
 	size_t f_msl = read_corpus(base + "." + f_lang, f_word2id, f_id2word, f_corpus);
@@ -188,11 +182,10 @@ void train(int iterations) {
 	}
 
 
-	// language model (not used as yet)
-	cout << "Generating language model...\n";
-	matrix<float> langmodel;
+	// language model
+	cerr << "Generating language model...\n";
 	langmodel.init(e_id2word.size() + 1, e_id2word.size() + 1);
-	for (auto sentence : e_corpus) {
+	for (vector<size_t>& sentence : e_corpus) {
 		size_t prev_id = e_id2word.size();
 		for (size_t id : sentence) {
 			langmodel[id][prev_id]++;
@@ -200,17 +193,16 @@ void train(int iterations) {
 		}
 		langmodel[e_id2word.size()][prev_id]++;
 	}
-	normalize(langmodel);
+	langmodel.normalize();
 
 
-	// length model (not used as yet)
-	cout << "Generating length model...\n";
-	matrix<float> lenmodel;
+	// length model
+	cerr << "Generating length model...\n";
 	lenmodel.init(f_msl, e_msl);
 	for (size_t l = 0; l < corpus_size; l++) {
 		lenmodel[f_corpus[l].size() - 1][e_corpus[l].size() - 1]++;
 	}
-	normalize(lenmodel);
+	lenmodel.normalize();
 
 
 	// dictionary training
@@ -220,9 +212,9 @@ void train(int iterations) {
 	dict.fill(1);
 
 	signal(SIGINT, leaving);
-	cout << "Training dictionary...\n";
+	cerr << "Training...\n";
 	for (int count = 0; count < iterations; count++) {
-		cout << "Step " << count + 1 << "...\n";
+		cerr << "Step " << count + 1 << "...\n";
 		c.zero();
 		for (size_t l = 0; l < corpus_size; l++) {
 			for (size_t f : f_corpus[l]) {
@@ -232,7 +224,7 @@ void train(int iterations) {
 				for (size_t e : e_corpus[l]) c[f][e] += dict[f][e] * s;
 			}
 		}
-		normalize(c);
+		c.normalize();
 		dict.swap(c);
 	}
 	save();
@@ -241,14 +233,140 @@ void train(int iterations) {
 
 void lookup() {
 	load();
-	cout << "Reading input...\n";
-	string s;
-	while (getline(cin, s)) top_ten(s);
+	cerr << "Looking up...\n";
+	string word;
+	while (getline(cin, word)) {
+		cout << word << "\n";
+		if (!f_word2id.count(word)) {
+			cout << "\tUnknown word.\n";
+			continue;
+		}
+		size_t id = f_word2id[word];
+		vector<size_t> top;
+		float s = 0;
+		for (size_t i = 0; i < e_id2word.size(); i++) {
+			top.push_back(i);
+			s += dict[id][i];
+		}
+		sort(top.begin(), top.end(), [&](size_t a, size_t b) {
+			return dict[id][a] > dict[id][b];
+		});
+		for (size_t i = 0; i < 10; i++) {
+			printf("\t%12.10f   %s\n", dict[id][top[i]] / s, e_id2word[top[i]].c_str());
+		}
+	}
+}
+
+
+float prob(const vector<size_t>& f_s, const vector<size_t>& e_s) {
+
+	//if (e_s.size() < 1) return 0;
+
+	float p = 1;
+	for (size_t i = 1; i < e_s.size(); i++) {
+		p *= langmodel[e_s[i]][e_s[i - 1]];
+	}
+
+	p /= pow(e_s.size() - 1 - (e_s.back() == e_id2word.size() - 1), f_s.size() - 2);
+
+
+	//p /= 1 + pow(f_s.size() - e_s.size(), 2);
+/*
+	if (f_s.size() - 3 < lenmodel.height() || e_s.size() - 3 < lenmodel.width()) {
+		p *= lenmodel[f_s.size() - 3][e_s.size() - 3];
+	}
+	else {
+		return 1;
+	}
+*/
+
+	for (size_t i = 1; i < f_s.size() - 1; i++) {
+		float s = 0;
+		for (size_t e : e_s) {
+			if (e != e_id2word.size() - 1) {
+				s += dict[f_s[i]][e];
+			}
+		}
+		p *= s;
+	}
+
+	return p;
+}
+
+
+void print_sentence(vector<size_t>& s) {
+	for (size_t i = 0; i < s.size(); i++) {
+		if (i) cout << " ";
+		cout << e_id2word[s[i]];
+	}
+	cout << "\n";
+}
+
+
+void decode() {
+	load();
+	cerr << "Decoding...\n";
+
+	string line;
+	while (getline(cin, line)) {
+		stringstream words(line);
+		vector<size_t> f_s;
+
+		string word;
+		while (words >> word) {
+			size_t id;
+			if (f_word2id.count(word) == 0) {
+				cerr << "Unknown word (" << word << ").\n";
+			} else id = f_word2id[word];
+			f_s.push_back(id);
+		}
+
+		vector<vector<size_t>> H;
+		vector<size_t> h;
+
+		do {
+			for (size_t e = 0; e < e_id2word.size(); e++) {
+
+				h.push_back(e);
+				H.push_back(h);
+				h.pop_back();
+				while (H.size() > 50) {
+
+					size_t j = 0;
+					float m = 9e9;
+					for (size_t i = 0; i < H.size(); i++) {
+						float s = prob(f_s, H[i]);
+						if (s < m) {
+							m = s;
+							j = i;
+						}
+					}
+					H.erase(H.begin() + j);
+				}
+			}
+
+			size_t j = 0;
+			float m = 0;
+			for (size_t i = 0; i < H.size(); i++) {
+				float s = prob(f_s, H[i]);
+				//cout << s << ": "; print_sentence(H[i]);
+				if (s > m) {
+					m = s;
+					j = i;
+				}
+			}
+			assert(m > 0);
+			h = H[j];
+			H.erase(H.begin() + j);
+
+		} while (h.back() != e_id2word.size() - 1);
+
+		print_sentence(h);
+	}
 }
 
 
 int main(int argc, char** argv) {
-
 	if (argc < 5) {
 		cout << "usage: " << argv[0] << " <base> <e> <f> <action> [iterations]\n";
 		return 0;
@@ -260,6 +378,7 @@ int main(int argc, char** argv) {
 
 	if (action == "train") train(argc > 5 ? atoi(argv[5]) : 100);
 	else if (action == "lookup") lookup();
+	else if (action == "decode") decode();
 	else {
 		cerr << "Invalid action.\n";
 		return 1;
